1f3154d694b1e2ccc001d6b4872f8a8b5cd3242c
Madeha Shahid <madehashahid@hotmail.com>
2018-10-18 17:21:41 -0400

    10/18 Update

--- server/index.js
+++ server/index.js
@@ -3,8 +3,15 @@ const game = require('./game/controller');
 
 const app = express();
 
+const port = 3000;
+const server = "localhost";
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use("/", express.static(__dirname + "/../dist/"));
 app.use('/game', game);
 
-app.listen(3000);
+app.listen(port);
+
+// eslint-disable-next-line no-console
+console.log(`listening on http://${server}:${port}`);

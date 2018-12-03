ffc0716010d75f4e601315e8b663554d2ed0088a
Madeha Shahid <madehashahid@hotmail.com>
2018-12-03 11:25:06 -0500

    PicturePicker

--- src/components/PicturePicker.vue
+++ src/components/PicturePicker.vue
@@ -1,22 +1,35 @@
 <template>
     <div>
-        <h1>The Picture Picker</h1>
+        <a  v-for="p in pictures" :key="p.id"
+            class="img-thumbnail">
+            <img :src="p.picture" />
+        </a>
     </div>
 </template>
-
 <script>
 export default {
     name: "PicturePicker",
-    data: function(){
-        
+    data: function () {
+        return{
+        }
     },
-    props: [pictures]
-    
+    props: {
+        pictures: Array
+    }
 }
 </script>
 
-
 <style lang="scss">
-
+    .img-thumbnail {
+        display: inline-block;
+        margin: 5px;
+        width: 75px;
+        height: 75px;
+        text-align: center;
+        img {
+            max-width: 95%;
+            max-height: 95%;
+            border-radius: 3px;
+        }
+    }
 </style>
-
